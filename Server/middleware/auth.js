bbca9c5b97a1a891500f0576bdf9e82535733364
shan <shanmondal007@gmail.com>
2025-09-14 16:41:14 +0530

    missImportAuth.js

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../Models/user.js';
+import User from '../Models/User.js';
 import cloudinary from '../lib/cloudinary.js';
 // Middleware to protect routes
 export const protectRoute = async (req, res, next) => {
